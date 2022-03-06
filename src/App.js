7c0f77fa5430a20a80dbf93f3b7239cd0c8db014
Oshoke <oyatioshoke@yahoo.com>
2022-03-05 20:38:53 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import "./index.css";
+import data from "./data";
 
 function App() {
+  const [count, setCount] = useState("");
+
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+
+    if (count > data.length) {
+      amount = data.length;
+    }
+    if (count <= 0) {
+      amount = 1;
+    }
+
+    setText(data.slice(0, amount));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section-center">
+      <h3>Tired of boring LOREM IPSUM?</h3>
+      <form onSubmit={handleSubmit}>
+        <label>paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          value={count}
+          onChange={(e) => {
+            setCount(e.target.value);
+          }}
+        />
+        <button>generate</button>
+      </form>
+
+      {text.map((item, index) => {
+        return <p key={index}>{item}</p>;
+      })}
+    </section>
   );
 }
 
